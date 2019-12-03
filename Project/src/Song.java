337b5659dd2bafb37291e6aeb54050db4d9c1b8d
hushion-r <shushion@macalester.edu>
2019-12-03 14:54:21 -0600

    Created classes

---
+++ Project/src/Song.java
@@ -0,0 +1,10 @@
+import java.util.Dictionary;
+import java.util.HashMap;
+
+public class Song {
+    String artist;
+    String song;
+    int length;
+
+    HashMap<Member, Integer> members = new HashMap<>();
+}
