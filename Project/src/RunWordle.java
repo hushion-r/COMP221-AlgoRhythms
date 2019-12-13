c0e3c8522f8d81481d5979c4cc6fe96d127a2f41
hushion-r <shushion@macalester.edu>
2019-12-13 12:40:13 -0600

    Trying to make graphics text expand and contract

--- Project/src/RunWordle.java
+++ Project/src/RunWordle.java
@@ -28,52 +28,67 @@ public class RunWordle {
         ////
         Group group1 = new Group("BTS");
         HashMap<String, Member> memberssss = new HashMap<>();
-        Song song1 = new Song("Run", group1);
-
 
         Member member1 = new Member("Jimin");
         member1.position = "vocalist";
         Member member2 = new Member("J-Hope");
         member2.position = "rapper";
-        Member member3 = new Member("Jungkook");
-        member3.position = "vocalist";
-        Member member4 = new Member("Jin");
-        member4.position = "vocalist";
+//        Member member3 = new Member("Jungkook");
+//        member3.position = "vocalist";
+//        Member member4 = new Member("Jin");
+//        member4.position = "vocalist";
 
         memberssss.put(member1.memberName, member1);
         memberssss.put(member2.memberName, member2);
-        memberssss.put(member3.memberName, member3);
-        memberssss.put(member4.memberName, member4);
+//        memberssss.put(member3.memberName, member3);
+//        memberssss.put(member4.memberName, member4);
 
         group1.members = memberssss;
+
+        Song song1 = new Song("Run", group1);
+        Song song2 = new Song("Hold Me Tight", group1);
 
         HashMap<Song, ArrayList<Integer>> songTimes = new HashMap<>();
         ArrayList<Integer> listy = new ArrayList<>();
         listy.add(60);  // seconds
-        listy.add(30);  // percentage
+        listy.add(60);  // percentage
         songTimes.put(song1, listy);
         member1.songTimes = songTimes;
 
         songTimes = new HashMap<>();
         listy = new ArrayList<>();
         listy.add(40);  // seconds
-        listy.add(20);  // percentage
+        listy.add(40);  // percentage
         songTimes.put(song1, listy);
         member2.songTimes = songTimes;
 
         songTimes = new HashMap<>();
         listy = new ArrayList<>();
-        listy.add(80);  // seconds
-        listy.add(40);  // percentage
-        songTimes.put(song1, listy);
-        member3.songTimes = songTimes;
+        listy.add(70);  // seconds
+        listy.add(70);  // percentage
+        songTimes.put(song2, listy);
+        member1.songTimes = songTimes;
 
         songTimes = new HashMap<>();
         listy = new ArrayList<>();
-        listy.add(20);  // seconds
-        listy.add(10);  // percentage
-        songTimes.put(song1, listy);
-        member4.songTimes = songTimes;
+        listy.add(30);  // seconds
+        listy.add(30);  // percentage
+        songTimes.put(song2, listy);
+        member2.songTimes = songTimes;
+
+//        songTimes = new HashMap<>();
+//        listy = new ArrayList<>();
+//        listy.add(80);  // seconds
+//        listy.add(40);  // percentage
+//        songTimes.put(song1, listy);
+//        member3.songTimes = songTimes;
+//
+//        songTimes = new HashMap<>();
+//        listy = new ArrayList<>();
+//        listy.add(20);  // seconds
+//        listy.add(10);  // percentage
+//        songTimes.put(song1, listy);
+//        member4.songTimes = songTimes;
         ////
 
         songsToCompare[0] = song1;
--- Project/src/Wordle.java
+++ Project/src/Wordle.java
@@ -16,6 +16,7 @@ public class Wordle extends GraphicsGroup {
     private double lowScore = 1.0;
 
     public Wordle(HashMap<String, Member> members, Song song, Color colorR, Color colorV, double x, double y) {
+//    public Wordle(HashMap<String, Member> members, Song[] songs, Color colorR, Color colorV, double x, double y) {
         super(x,y);
         rgen = new Random();
 
@@ -34,6 +35,19 @@ public class Wordle extends GraphicsGroup {
             }
             if (i<members.size()-1) i++;
         }
+
+//        int j = 0;
+//        for (Song s : songs) {
+//            for (Map.Entry<String, Member> entry : members.entrySet()) {
+//                labels[i] = new WordleGLabel(entry.getValue(), s, highScore, lowScore);
+//                if (entry.getValue().position.equals("rapper")) {
+//                    labels[i].setStrokeColor(colorR);
+//                } else if (entry.getValue().position.equals("vocalist")) {
+//                    labels[i].setStrokeColor(colorV);
+//                }
+//                if (i<members.size()-1) j++;
+//            }
+//        }
     }
 
     /**
