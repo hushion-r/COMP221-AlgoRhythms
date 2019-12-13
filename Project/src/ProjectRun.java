a96f5bf248bf862edd7d40918bd0cd69a0b73453
Bernadette Clermont <bclermon@macalester.edu>
2019-12-13 11:44:37 -0600

    Updated it so it successfully reads the file w/o errors :)

--- Project/src/ProjectRun.java
+++ Project/src/ProjectRun.java
@@ -19,30 +19,31 @@ public class ProjectRun {
 
     static void processFile(InputStream in) {
 
-        Scanner scan = new Scanner(in).useDelimiter("\n");
-
-        while(scan.hasNextLine()){
-            String song1 = scan.next();
-            Scanner sc = new Scanner(song1).useDelimiter(",");
+        Scanner scan = new Scanner(in).useDelimiter(";");
+        String currLine = scan.next();
+        while(scan.hasNext()) {
+            Scanner sc = new Scanner(currLine).useDelimiter(",");
             Group currGroup;
             boolean init = false;
 
             String groupName = sc.next();                                 //Make new group if we haven't encountered them
-            if(!Group.sampleGroups.containsKey(groupName)){
+            if (!Group.sampleGroups.containsKey(groupName)) {
                 init = true;
-               currGroup = Group.addGroup(groupName);
-               //System.out.println(groupName);
-            }
-            else{
+                currGroup = Group.addGroup(groupName);
+                System.out.println(groupName);
+            } else {
                 currGroup = Group.sampleGroups.get(groupName);              //else get the group object from someplace else
             }
 
-            Song newSong = new Song(sc.next(), currGroup);                //makes new song object
-            while(scan.hasNext()){
+            Song newSong = new Song(sc.next(), currGroup);//makes new song object
+            System.out.println(newSong.songName);
+            while (sc.hasNext()) {
                 currGroup.addTimes(newSong, sc.next(), init);
             }
+            sc.close();
+            currLine = scan.next();
         }
-//
+        scan.close();
 //
 //                /* READ UNTIL "."
 //            Read all until ","
