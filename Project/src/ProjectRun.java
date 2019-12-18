8f0953975feca8b9d7b3dcd36a00353e998b3216
hushion-r <shushion@macalester.edu>
2019-12-17 21:05:19 -0600

    Working

--- Project/src/ProjectRun.java
+++ Project/src/ProjectRun.java
@@ -14,7 +14,7 @@ public class ProjectRun {
     }
 
     static void processFile(InputStream in) {
-
+        //ffffffff
         Scanner scan = new Scanner(in).useDelimiter(";");
         String currLine = scan.next().trim();
         currLine = currLine.replaceAll("\r\n", "");
