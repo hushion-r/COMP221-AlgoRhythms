c80cd02818b7ce950ecd25d7729f6599ebbce6de
Bernadette Clermont <bclermon@macalester.edu>
2019-12-17 20:26:14 -0600

    Added more groups to the soup text

--- Project/src/DisplayWordle.java
+++ Project/src/DisplayWordle.java
@@ -5,12 +5,16 @@ import java.awt.*;
 
 public class DisplayWordle extends JFrame {
 
-    Group default1;
-    Group default2;
+    private Group default1;
+    private Group default2;
 
     public DisplayWordle(String title) {
-         default1 = Group.sampleGroups.get(0);
-         default2 = Group.sampleGroups.get(1);
+        ProjectRun.processFile("soup.txt");
+
+        default1 = Group.sampleGroups.get("GOT7");
+        default2 = Group.sampleGroups.get("BTS");
+
+        System.out.println(Group.sampleGroups.toString());
 
         new JFrame(title);
         createBanner();
@@ -26,9 +30,11 @@ public class DisplayWordle extends JFrame {
 
     public void createBanner(){
         JPanel bothGroups = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        System.out.print("Group I'm trying to include" + default1.groupName);
 
         JPanel firstGroup = new JPanel();
         JLabel firstName = new JLabel(default1.groupName);
+
         firstGroup.add(firstName);
         //Song list for the first group
         JList songList1 = new JList(default1.allSongs.toArray());
@@ -36,6 +42,7 @@ public class DisplayWordle extends JFrame {
         bothGroups.add(firstGroup);
 
         JPanel secondGroup = new JPanel();
+        System.out.print("Second Group I'm trying to include" + default2.groupName);
         JLabel secondName = new JLabel(default2.groupName);
         secondGroup.add(secondName);
         //list of songs for second group
